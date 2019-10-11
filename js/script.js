a7cd7efc1662ac7c8672732ef20627734f42bb49
mingyong.dev <mingyong.dev@gmail.com>
2019-10-11 01:29:26 -0400

    practice tab

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 const filter_work = document.querySelector(".works");
 const buildNum = document.querySelector(".buildNum");
 const writeNum = document.querySelector(".writeNum");
-const allNum = document.querySelector(".allNum");
+const practiceNum = document.querySelector(".practiceNum");
 const projectsBtn = document.querySelector(".btn_building");
 const articlesBtn = document.querySelector(".btn_writing");
-const allBtn = document.querySelector(".btn_all");
-let build_data, build_response, write_data, write_response, allResponse;
+const practiceBtn = document.querySelector(".btn_practice");
+let build_data, build_response, write_data, write_response, practice_response, practice_data;
 
 async function getData() {
 	build_data = await fetch("./js/building.json");
 	build_response = await build_data.json();
 	write_data = await fetch("./js/writing.json");
 	write_response = await write_data.json();
+	practice_data = await fetch("./js/practice.json");
+	practice_response = await practice_data.json();
 	return {
 		buildNum: build_response.length,
 		writeNum: write_response.length,
-		allNum: write_response.length + build_response.length,
-		allResponse: [...build_response, ...write_response]
+		practiceNum: practice_response.length,
+		response: [...write_response, ...build_response, ...practice_response]
 	};
 }
 
@@ -25,13 +27,15 @@ async function generateCard(response) {
 	filter_work.innerHTML = response
 		.map(result => {
 			return `
-    <a href="${result.link}" class="div_build col-sm-12 col-lg-3 col-xl-2">
-    <img src=${result.img} alt=${result.alt} />
-    <div class="div_info">
-      <p class="p_name">${result.name}</p>
-      <p class="p_description">${result.description}</p>
-    </div>
-    </a>
+		<div class="div_builds col-sm-12 col-lg-3 col-xl-2 my-3">
+			<a href="${result.link}" class="div_build">
+			<img src=${result.img} alt=${result.alt}/>
+			<div class="div_info">
+				<p class="p_name">${result.name}</p>
+				<p class="p_description">${result.description}</p>
+			</div>
+			</a>
+		</div>
   `;
 		})
 		.join("");
@@ -42,11 +46,14 @@ getData()
 	.then(result => {
 		buildNum.textContent = result.buildNum;
 		writeNum.textContent = result.writeNum;
-		allNum.textContent = result.allNum;
-		allResponse = result.allResponse;
+		practiceNum.textContent = result.practiceNum;
+		allResponse = result.response;
 		generateCard(allResponse);
 	})
-	.catch(err => alert("Error: " + err + ". Opps, something went wrong, please contact Ming."));
+	.catch(err => {
+		console.error(err);
+		alert("Error: " + err + ". Opps, something went wrong, please contact Ming.");
+	});
 
 //filter if buttons are clicked
 projectsBtn.addEventListener("click", () => {
@@ -57,6 +64,6 @@ articlesBtn.addEventListener("click", () => {
 	generateCard(write_response);
 });
 
-allBtn.addEventListener("click", () => {
-	generateCard(allResponse);
+practiceBtn.addEventListener("click", () => {
+	generateCard(practice_response);
 });
